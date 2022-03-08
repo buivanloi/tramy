132eece148ad562489f016851c419681630bdbc8
buivanloi <100933683+buivanloi@users.noreply.github.com>
2022-03-08 16:37:40 +0700

    Add files via upload
    hi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '8/3';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'An & Lợi tặng My';
+const contentCard = 'An và Lợi chúc Trà My ngày 8/3 vui vẻ, tràn đầy sức khỏe, ngày càng xinh đẹp với cả sớm thành cô bác sĩ. À còn quà 8/3 của Tramy đâu thì nó đang được vận chuyển qua đường bưu điện tới trọ nhea, của ít lòng nhiều đây cũng là món đầu tiên mà 2 đứa tặng cho m. Thế nhé, hết rồi đó. 8/3 vui vẻ.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'tramy.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
